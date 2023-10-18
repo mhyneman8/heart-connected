defcb7f1c31cbe2ae957610af9a0fcfd65c28e22
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-10-17 19:39:10 -0500

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'teal': '#84ceeb',
+        'purple': '#8860d0',
+        'indigo': '#c1c8e4',
+        'cyan': '#5ab9ea',
+        'blue': '#5680e9',
+        'black': '#191716'
+      }
     },
   },
   plugins: [],
