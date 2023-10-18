be91fa6df22383ddab9254d375fc85f50c57e83d
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-10-17 19:40:56 -0500

    network page

---
+++ pages/network.tsx
@@ -0,0 +1,45 @@
+import Heading from "@/components/Heading";
+import Navigation from "@/components/Navigation";
+import Modal from "@/components/NetworkModal";
+
+
+// command to start mongodb: brew services start mongodb-community
+// command to stop mongoDB: brew services stop mongodb-community
+// launch the shell: mongosh
+// list all databases: show dbs
+// which database MongoDB is currently in: db
+// create new DB or switch to DB: use [database name]
+// heartsConnectedDB
+// view all collections in db: db.getCollectionNames()
+// create a new collection: db.createCollection("users")
+// insert new document into collection: db.[collectionName].insertOne(document-to-insert)
+// read documents: db.[collectionName].find()
+// find document that meets condition: db.[collectionName].find( [Condition] ): ex: db.users.find( {"Location.City": "Chicago"})
+// update field: db.users.updateOne( {"Name.First": "Mallory"}, { $set: { "Location.city": "Tulsa"}})
+// add new field: db.users.update( [condition of which items to update], { $push: { [name of key to add]: "value"} })
+// delete 1 collection: db.[collectionName].deleteOne({ "Genre.Name": "Comedy" })
+// delete multiple collections: db.[collectionName].deleteMany({ "Genre.Name": "Comedy" })
+// 
+
+export default function Network() {
+    return (
+        <div>
+            <Navigation />
+            <Heading heading="Join Our Community"/>
+            <div className="body">
+                {/* <div className="cta w-full text-center my-20">
+                    <div className="rounded-full w-fit m-auto text-white bg-cyan px-10 py-5 hover:opacity-70">
+                        Join Our Community
+                    </div>
+                </div> */}
+                <Modal />
+                <div>
+                    <div>Benefits of joining</div>
+                    <ul>
+                        <li>Connect with others in the CHD family</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
