2bc240bf3675f123e188961f6d02292b73f31b75
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-10-17 19:41:41 -0500

    calculator init

---
+++ pages/calculator.tsx
@@ -0,0 +1,12 @@
+import Heading from "@/components/Heading";
+import Navigation from "../components/Navigation";
+
+export default function Calculator() {
+    return (
+        <div>
+            <Navigation />
+
+            <Heading heading="Calculator" />
+        </div>
+    )
+}
