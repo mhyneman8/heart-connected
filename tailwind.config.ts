990474dbe6c963e628742e0572df5d4709b00701
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-11-05 16:00:38 -0600

    update linting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        'teal': '#84ceeb',
-        'purple': '#8860d0',
-        'indigo': '#c1c8e4',
-        'cyan': '#5ab9ea',
-        'blue': '#5680e9',
-        'black': '#191716'
-      }
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				teal: '#84ceeb',
+				purple: '#8860d0',
+				indigo: '#c1c8e4',
+				cyan: '#5ab9ea',
+				blue: '#5680e9',
+				black: '#191716',
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
