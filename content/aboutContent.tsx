1d9598443ea2a4fa83fbe3378c8f5e95e1f2f3a4
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-11-05 16:02:13 -0600

    Pull out about content

---
+++ content/aboutContent.tsx
@@ -0,0 +1,23 @@
+interface AboutContent {
+	heading?: string;
+	content: string;
+}
+
+const aboutContent: AboutContent[] = [
+	{
+		content:
+			'One percent of all children are born with a congenital heart disease. Yet, we often use words like “rare” to define them leaving parents and families isolated and helpless. Whether the disease is classified as mild or severe, CHD parents are at risk of developing a traumatic response just from being told the diagnosis, let alone the other events that are often associated with CHD care. Our hope through the development of this website is to build a local and expand on the national community around CHD so no parent, child, or sibling feels alone. The congenital heart disease community is strong and supportive, but finding the right resources and sifting through all the information out there can be difficult. We hope that this website can serve as a central hub that promotes the already fantastic organizations that have been tireless working to improve care, for both the patients and families of CHD. Through education, compassion, and a united community, we aim to transform fear into resilience, uncertainty into hope, and isolation into connection. Together, we envision a world where the journey of CHD is met with understanding, strength, and unwavering support.',
+	},
+	{
+		heading: 'Our Mission:',
+		content:
+			'Through education, compassion, and a united community, we aim to transform fear into resilience, uncertainty into hope, and isolation into connection. Together, we envision a world where the journey of CHD is met with understanding, strength, and unwavering support.',
+	},
+	{
+		heading: 'Our Vision:',
+		content:
+			'Our vision at Heart Connected is a world transformed by compassion, knowledge, and unity. We envision a future where the burden of congenital Heart Disease (CHD) is significantly reduced, and individuals and families facing this diagnosis find solace in a community that understands, empowers, and supports them. In this world, the trauma response to a CHD diagnosis is replaced with resilience, hope, and strength. We see a society where every child born with CHD has access to the best possible care, where their families are equipped with the information and resources they need to make informed decisions, and where the journey of CHD is met with unwavering support and camaraderie. With your partnership, we are working tirelessly to turn this vision into reality, one heart at a time.',
+	},
+];
+
+export default aboutContent;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,40 +1,46 @@
-import Navigation from '../components/Navigation';
-import Heading from '../components/Heading';
+import Navigation from '@/components/Navigation';
+import Heading from '@/components/Heading';
+import React, { useState } from 'react';
+import aboutContent from '@/content/aboutContent';
+import NetworkModal from '@/components/NetworkModal';
 
 export default function About() {
-    return (
-    <div>
-        <Navigation />
-        <Heading heading="About" />
-        <div className="body flex flex-col justify-center mb-10">
-            <div className="mission m-auto w-4/5">
-                <div className="font-bold text-4xl mb-5">Our Mission:</div>
-                <div>
-                    Through education, compassion, and a united community, we aim 
-                    to transform fear into resilience, uncertainty into hope, and isolation 
-                    into connection. Together, we envision a world where the journey of CHD 
-                    is met with understanding, strength, and unwavering support.
-                </div>
-            </div>
-            <div className="vision mt-10 m-auto w-4/5">
-                <div className="font-bold text-4xl mb-5">Our Vision:</div>
-                <div>
-                    Our vision at Heart Connected is a world transformed by compassion, 
-                    knowledge, and unity. We envision a future where the burden of 
-                    congenital Heart Disease (CHD) is significantly reduced, and individuals 
-                    and families facing this diagnosis find solace in a community that 
-                    understands, empowers, and supports them. In this world, the trauma 
-                    response to a CHD diagnosis is replaced with resilience, hope, and 
-                    strength. We see a society where every child born with CHD has access 
-                    to the best possible care, where their families are equipped with the 
-                    information and resources they need to make informed decisions, and where 
-                    the journey of CHD is met with unwavering support and camaraderie. With 
-                    your partnership, we are working tirelessly to turn this vision into 
-                    reality, one heart at a time.
-                </div>
-            </div>
-        </div>
-    </div>
+	const [showModal, setShowModal] = React.useState<boolean>(false);
 
-    )
+	return (
+		<div>
+			<Navigation />
+			<Heading heading='About Heart Connected' />
+			<div className='body flex flex-col justify-center mb-10'>
+				{aboutContent.map((section, index) => (
+					<div
+						key={index}
+						className='m-auto w-4/5 mt-8'
+					>
+						<div className='font-bold text-4xl mb-5'>{section.heading}</div>
+						<div>{section.content}</div>
+					</div>
+				))}
+
+				<div className='flex flex-col justify-center'>
+					{showModal && (
+						<NetworkModal
+							showModal={showModal}
+							setShowModal={setShowModal}
+						/>
+					)}
+					<div className='h2 w-3/4 m-auto text-5xl mt-12 mb-8'>
+						Connect with a family like yours
+					</div>
+					<button
+						className='text-white font-bold px-8 py-3 rounded-full m-auto w-fit bg-cyan shadow hover:shadow-lg outline-none focus:outline-none my-4'
+						type='button'
+						onClick={() => setShowModal(true)}
+					>
+						Join Now
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
