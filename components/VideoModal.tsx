3680db32f1d2dcf9793d1e014de1ff2bb0194e7c
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-11-11 22:30:03 -0600

    Remove youtube react

--- components/VideoModal.tsx
+++ components/VideoModal.tsx
@@ -1,27 +1,26 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import YouTubePlayer from '@/components/YouTubePlayer';
 
 type Props = {
-  // showVideo: boolean,
   setShowVideo: React.Dispatch<React.SetStateAction<boolean>>,
-  content: string,
+  credit: string,
   video: string,
   title?: string,
 }
 
-export default function NetworkModal({ setShowVideo, content, video: videoId, title }: Props) {
-  console.log(videoId)
+export default function VideoModal({ setShowVideo, credit, video: videoId, title }: Props) {
+  const url = 'https://www.youtube.com/embed/' + videoId;
+  
   return (
     <div className='text-center'>
       <div className="opacity-40 bg-black fixed inset-0 z-40"></div>
       <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative my-6 mx-auto">
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+            style={{ minWidth: '80vw', minHeight: '60vh' }}
+          >
             <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
                 <h3 className="text-3xl font=semibold">
                   {title}
-                  {videoId}
                 </h3>
                 {/* close button */}
                 <button
@@ -33,11 +32,21 @@ export default function NetworkModal({ setShowVideo, content, video: videoId, ti
                   </span>
                 </button>
             </div>
-            <div className="relative p-6 flex-auto">
-              <YouTubePlayer videoId={videoId} />
+            <div className="relative p-6 flex justify-center">
+              <iframe 
+                width="560" 
+                height="315" 
+                src={url}
+                frameBorder="0" 
+                allow="data-allow" 
+                allowFullScreen
+              />
             </div>
         
-            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+            <div className=" p-6 border-t flex justify-between items-center border-solid border-blueGray-200 rounded-b">
+              <div className='font-bold text-sm'>
+                Video by: {credit}
+              </div>
               <button
                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
                 type="button"
