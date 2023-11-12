18c8a4d6bc8d7ffd560f671a8a2fdd8470436386
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-11-11 22:30:27 -0600

    Add images to each hero

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,13 +1,26 @@
-// import config from "@/tailwind.config";
+import Image from "next/image";
 
 type Heading = {
     heading: string;
+    imgSrc: string;
 }
 
-export default function Heading({ heading }: Heading) {
+export default function Heading({ heading, imgSrc }: Heading) {
     return (
-        <div className="hero flex flex-col justify-center mb-20">
-            <div className="title text-7xl mx-auto mt-32 mb-16 text-center">{heading}</div>
+        // <div className="hero flex flex-col justify-center mb-20">
+        <div className='hero h-full min-h-[70vh] flex flex-col justify-end relative mb-20'>
+            <Image
+					src={imgSrc}
+					fill={true}
+					alt=''
+				/>
+                <div
+					className='title mx-auto mb-8 lg:text-6xl md:text-5xl text-4xl max-w-xl text-center flex align-end text-white px-3 z-10 bg-sky-400/50 rounded'
+					style={{ boxShadow: '0 0 10px 26px rgb(56 189 248 / .5)' }}
+				>
+                    {heading}
+				</div>
+            {/* <div className="title text-7xl mx-auto mt-32 mb-16 text-center">{heading}</div> */}
         </div>
     )
 }
