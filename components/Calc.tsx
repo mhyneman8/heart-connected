73ae577bb73d3f1dd9c1bffd122fbd4c881bfe4d
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-11-18 14:26:29 -0600

    Style calculator

--- components/Calc.tsx
+++ components/Calc.tsx
@@ -1,91 +1,115 @@
-import { ReactEventHandler, useState } from "react";
+import { ReactEventHandler, useState } from 'react';
 import RiskAnswer from '@/components/RiskAnswers';
-import calcQuestions from "@/content/calculatorQuestions";
+import calcQuestions from '@/content/calculatorQuestions';
 
+type Props = {
+	setShowCalculator: React.Dispatch<React.SetStateAction<boolean>>;
+};
+export default function Calc({ setShowCalculator }: Props) {
+	const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<null | number>(null);
+	const [score, setScore] = useState<number>(0);
+	const [showResult, setShowResult] = useState(false);
+	const [activeQuestion, setActiveQuestion] = useState(0);
+	const [selectedAnswer, setSelectedAnswer] = useState(0);
 
-export default function Calc() {
-    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState<null | number>(null);
-    const [score, setScore] = useState<number>(0);
-    const [showResult, setShowResult] = useState(false);
-    const [activeQuestion, setActiveQuestion] = useState(0);
-    const [selectedAnswer, setSelectedAnswer] = useState(0);
+	const questions = calcQuestions;
+	const { question, choices, questionNumber } = questions[activeQuestion];
 
-    const questions = calcQuestions;
-    const {question, choices, questionNumber} = questions[activeQuestion];
+	const selectedAnswerStyles = 'bg-indigo border-purple w-fit px-5 mb-4 py-1 rounded hover:cursor-pointer';
 
-    const selectedAnswerStyles = 'bg-indigo border-1 border-purple'
+	const handleSelected = (value: number, index: number) => {
+		setSelectedAnswer(value);
+		setSelectedAnswerIndex(index);
+	};
 
-    const handleSelected = (value: number, index: number) => {
-        setSelectedAnswer(value)
-        setSelectedAnswerIndex(index)
-    }
+	const onClickNext = () => {
+		setScore(selectedAnswer + score);
+		setSelectedAnswerIndex(null);
 
-    const onClickNext = () => {
-        setScore(selectedAnswer + score)
-        setSelectedAnswerIndex(null)
+		if (activeQuestion !== questions.length - 1) {
+			setActiveQuestion((prev) => prev + 1);
+		} else {
+			setActiveQuestion(0);
+			setShowResult(true);
+		}
+	};
 
-        if (activeQuestion !== questions.length -1) {
-            setActiveQuestion((prev) => prev + 1)
-        } else {
-            setActiveQuestion(0)
-            setShowResult(true)
-        }
-    }
+	const startOver = () => {
+		setSelectedAnswerIndex(null);
+		setScore(0);
+		setActiveQuestion(0);
+		setSelectedAnswer(0);
+		setShowResult(false);
+	};
 
-    const startOver = () => {
-        setSelectedAnswerIndex(null);
-        setScore(0);
-        setActiveQuestion(0);
-        setSelectedAnswer(0);
-        setShowResult(false);
-    }
+	const handleClose = () => {
+		document.querySelector('body')!.style.overflow = 'auto';
+		setShowCalculator(false);
+	};
 
-    return (
-        <div className='flex flex-col bg-white w-screen h-screen justify-center items-center relative'>
-            <div className='opacity-40 bg-black fixed inset-0 z-40' ></div>
-            <div className='absolute top-0 overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
-                {showResult ? (
-                    <div>
-                        <RiskAnswer score={score} />
-                        Total: {score}
-
-                        <button onClick={() => startOver()}>
-                            Start Over
-                        </button>
-                    </div>
-                ) : (
-                    <div className=''>
-                        {/* <button onClick={() => previousQuestion()}>
+	return (
+		<div className='flex flex-col bg-white h-screen relative top-0 justify-center items-center relative  w-[80vw] md:w-full'>
+			<div className='opacity-40 bg-black fixed inset-0 z-40 overflow-hidden'></div>
+			<div className='overflow-y-auto fixed max-w-[520px] inset-0 z-50 outline-none focus:outline-none border-0 my-10 mx-auto w-max-[500px] p-8 rounded-lg shadow-lg flex flex-col bg-white outline-none focus:outline-none'>
+				<button
+					className='absolute right-20'
+					onClick={() => handleClose}
+				>
+					X
+				</button>
+				<h1 className='indent-0 font-bold text-lg mb-3'>Risk Level Estimator</h1>
+				{showResult ? (
+					<div className='mt-8 indent-0'>
+						<RiskAnswer score={score} />
+						{/* Total (for testing purposes): {score} */}
+						<div className='w-full flex justify-center items-center mt-10'>
+							<button
+								className='text-white font-bold px-8 py-3 rounded-full m-auto bg-cyan shadow hover:shadow-lg outline-none focus:outline-none mb-4'
+								onClick={() => startOver()}
+							>
+								Start Over
+							</button>
+						</div>
+					</div>
+				) : (
+					<div className=''>
+						{/* <button onClick={() => previousQuestion()}>
                             Previous question
                         </button> */}
 
-                        <h1>Quiz</h1>
-                        <h2> {questionNumber}.) {question}</h2>
-                        <ul>
-                            {choices.map((item, index) => (
-                                <li 
-                                    onClick={() => handleSelected(item[1], index)} 
-                                    key={index}
-                                    className={selectedAnswerIndex === index ? selectedAnswerStyles : ''}
-                                >
-                                    {item[0]}
-                                </li>
-                            ))}
-                        </ul>
-                        <button 
-                            onClick={() => onClickNext()}
-                            disabled={selectedAnswerIndex === null}
-                        >
-                            {activeQuestion === questions.length-1 ? 'Finish': 'Next'}
-                        </button>
-                    </div>
-                )}
-                <div className="caption">
-                    To better help us formulate an accurate risk factor please consider, also filling out 
-                    <a href="">our survey</a>.
-                </div>
-
-            </div>
-        </div>
-    )
+						<div className='indent-0 font-bold text-xs mb-3'>Question {questionNumber} / 9</div>
+						<h2 className='indent-0 mb-4'>{question}</h2>
+						<ul className='list-disc indent-0 ml-6'>
+							{choices.map((item, index) => (
+								<li
+									onClick={() => handleSelected(item[1], index)}
+									key={index}
+									className={
+										selectedAnswerIndex === index ? selectedAnswerStyles : 'px-5  py-1 mb-4 hover:cursor-pointer'
+									}
+								>
+									{item[0]}
+								</li>
+							))}
+						</ul>
+						<div className='w-full flex justify-center items-center'>
+							<button
+								onClick={() => onClickNext()}
+								disabled={selectedAnswerIndex === null}
+								className='bg-cyan py-1 px-7 text-white hover:opacity-8 rounded'
+							>
+								{activeQuestion === questions.length - 1 ? 'Finish' : 'Next'}
+							</button>
+						</div>
+					</div>
+				)}
+				<div className='flex justify-center '>
+					<div className='caption absolute bottom-10 sm:bottom-4 max-w-sm text-center m-auto indent-0'>
+						To better help us formulate an accurate risk factor please consider also filling out {''}
+						<a href=''>our survey</a>.
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- content/calculatorQuestions.tsx
+++ content/calculatorQuestions.tsx
@@ -1,96 +1,95 @@
-type Choices = [
-    string, number
-]
+type Choices = [string, number];
 
 interface CalcQuestions {
 	question: string;
-    questionNumber: number;
-    choices: Choices[]
+	questionNumber: number;
+	choices: Choices[];
 }
 
 const calcQuestions: CalcQuestions[] = [
 	{
-        question: 'Define your relationship to your family member with CHD.',
-        questionNumber: 1,
-        choices: [
-            ['Mother', 1],
-            ['Father', 0],
-            ['Sibling', 0],
-            ['Extended Family', 0],
-        ]
+		question: 'Which of the following defines your relationship to your family member with CHD.',
+		questionNumber: 1,
+		choices: [
+			['Mother', 1],
+			['Father', 0],
+			['Sibling', 0],
+			['Extended Family', 0],
+		],
 	},
 	{
-        question: 'What is your current age?',
-        questionNumber: 2,
-        choices: [
-            ['Below 25', 0],
-            ['26-35', 0],
-            ['36 or older', 2],
-        ]
+		question: 'What is your current age?',
+		questionNumber: 2,
+		choices: [
+			['Below 25', 0],
+			['26-35', 0],
+			['36 or older', 2],
+		],
 	},
 	{
-        question: 'What is your current employment status?',
-        questionNumber: 3,
-        choices: [
-            ['Employed with benefits', 0],
-            ['Employed without benefits', 0],
-            ['Self-employed (Self-pay benefits)', 0],
-            ['Not currently employed', 2],
-        ]
+		question: 'What is your current employment status?',
+		questionNumber: 3,
+		choices: [
+			['Employed with benefits', 0],
+			['Employed without benefits', 0],
+			['Self-employed (Self-pay benefits)', 0],
+			['Not currently employed', 2],
+		],
 	},
 	{
-        question: 'Please select the esitmated yearly household income.',
-        questionNumber: 4,
-        choices: [
-            ['Less than 24k', 3],
-            ['24k - 75k', 0],
-            ['75k-121k', 0],
-            ['More than 121k', 0],
-        ]
+		question: 'Please select the esitmated yearly household income.',
+		questionNumber: 4,
+		choices: [
+			['Less than 24k', 3],
+			['24k - 75k', 0],
+			['75k-121k', 0],
+			['More than 121k', 0],
+		],
 	},
 	{
-        question: 'Based on the provided classification, what severity of CHD does your family member have?',
-        questionNumber: 5,
-        choices: [
-            ['Mild', 1],
-            ['Moderate', 2],
-            ['Severe', 3],
-        ]
+		question: 'Based on the provided classification, what severity of CHD does your family member have?',
+		questionNumber: 5,
+		choices: [
+			['Mild', 1],
+			['Moderate', 2],
+			['Severe', 3],
+		],
 	},
 	{
-        question: 'How much time have you and/or your family member with CHD spend in the Intensive Care Unt since birth?',
-        questionNumber: 6,
-        choices: [
-            ['0-1 week', 1],
-            ['1-2 weeks', 2],
-            ['more than 2 weeks', 3],
-        ]
+		question: 'How much time have you and/or your family member with CHD spend in the Intensive Care Unt since birth?',
+		questionNumber: 6,
+		choices: [
+			['0-1 week', 1],
+			['1-2 weeks', 2],
+			['more than 2 weeks', 3],
+		],
 	},
 	{
-        question: 'Has your family member had or are they expected to require surgical correction of their heart lesion?',
-        questionNumber: 7,
-        choices: [
-            ['Yes', 2],
-            ['No', 0],
-        ]
+		question: 'Has your family member had or are they expected to require surgical correction of their heart lesion?',
+		questionNumber: 7,
+		choices: [
+			['Yes', 2],
+			['No', 0],
+		],
 	},
 	{
-        question: 'For caretakers only: Have you experienced difficulties with breastfeeding your child?',
-        questionNumber: 8,
-        choices: [
-            ['Yes', 1],
-            ['No', 0],
-            ['N/A', 0],
-        ]
+		question: 'For caretakers only: Have you experienced difficulties with breastfeeding your child?',
+		questionNumber: 8,
+		choices: [
+			['Yes', 1],
+			['No', 0],
+			['N/A', 0],
+		],
 	},
 	{
-        question: 'Birthing parent only: Have you been diagnosed with postpartum depression after the birth of your child with CHD?',
-        questionNumber: 9,
-        choices: [
-            ['Yes', 2],
-            ['No', 0],
-            ['N/A', 0],
-        ]
+		question:
+			'Birthing parent only: Have you been diagnosed with postpartum depression after the birth of your child with CHD?',
+		questionNumber: 9,
+		choices: [
+			['Yes', 2],
+			['No', 0],
+			['N/A', 0],
+		],
 	},
 ];
 
