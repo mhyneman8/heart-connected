a270a6dced426dc4ae61bc51fb6b799f2057ac11
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-11-30 16:28:13 -0600

    Remove unused items

--- next.config.js
+++ next.config.js
@@ -1,45 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.ucl.ac.uk',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.mechanobio.info',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'upload.wikimedia.org',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.mayoclinic.org',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.heart.org',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.tourmyindia.com',
-        port: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.mountsinai.org',
-        port: '',
-      },
-    ],
-  },
+
 }
 
 module.exports = nextConfig
